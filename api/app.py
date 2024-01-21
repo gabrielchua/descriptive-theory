98b0d97914cb5f17e19c7f86c972e6f2d111edf5
Gabriel Chua <cyzgab@gmail.com>
2024-01-21 16:48:27 +0800

    update error code

--- api/app.py
+++ api/app.py
@@ -8,13 +8,16 @@ from openai import OpenAI
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
-DEFAULT_SYSTEM_MESSAGE = "You are a friendly doctor. "\
+SIMPLIFY_SYSTEM_MESSAGE = "You are a friendly doctor. "\
             "Please simplify the given medical report in plain English. "\
             "Explain the findings, and if they are not serious, "\
             "assure the patient. "\
             "Be succicnt, and do not use technical terms someone without a "\
             "medical or life science degree would not understand. "\
             "Never add emojis or code snippets. "
+
+GUARDRAIL_SYSTEM_MESSAGE = "Your goal is to classify if the given text is about medical matters. If yes, return 1. If no, return 0"
+
 
 # Initialise clients
 app = FastAPI()
@@ -35,6 +38,7 @@ class SimplifyTextRequest(BaseModel):
 
 # GET endpoint to serve as a GET ping
 @app.get("/")
+@app.get("/ping")
 async def ping():
     """
     Endpoint to check if the server is running.
@@ -49,8 +53,8 @@ async def ping():
             }
 
 # POST endpoint to simplify text
-@app.post("/simplify-text")
-async def simplify_text(request: SimplifyTextRequest):
+@app.post("/simplify")
+async def simplify(request: SimplifyTextRequest):
     """
     Endpoint to simplify the given text based on the specified language.
     It accepts a JSON payload conforming to the SimplifyTextRequest model.
@@ -61,41 +65,73 @@ async def simplify_text(request: SimplifyTextRequest):
     Returns:
         JSON response with the simplified text.
     """
-    simplified_report = simplify(original_message=request.text,
+    if not scan_input(request.text):
+        return {"code": 490,
+            "message": "Please only use this API to simplify medical text."
+            }
+    simplified_report = simplify_text(original_message=request.text,
                                language=request.language)
-    
     if simplified_report:
-        simplified_text = "Hello - here is the report from today's visit: " + simplified_report
+        simplified_text = simplified_report
         return {"code": 200,
                 "message": {"simplified_text": simplified_text}
                 }
+    return {"code": 500,
+            "message": "Unexpected error."
+            }
+  
+def scan_input(prompt_to_check):
+    """
+    Scans input. Returns 0 if not medical related, and 1 otherwise.
+    """
+    result = int(_binary_classifier(GUARDRAIL_SYSTEM_MESSAGE, prompt_to_check))
+    return result
     
-    else:
-        return {"code": 500,
-                "message": "Unexpected error."
-                }
-    
-def simplify(original_message:str, language:str) -> Optional[str]:
+def simplify_text(original_message:str, language:str) -> Optional[str]:
     """
     Simplifies text
     """
-    system_prompt = _generate_system_prompt(language)
+    system_message = _generate_system_prompt(language)
 
-    response = client.chat.completions.create(
-        model="gpt-4-1106-preview",
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": original_message}
-        ],
-        temperature=0,
-        seed=0
-    )
-
-    return response.choices[0].message.content
-
+    response = _chat_completion(model="gpt-4-1106-preview",
+                                system_message=system_message, 
+                                prompt=original_message)
+    
+    return response
 
 def _generate_system_prompt(language:str) -> str:
     """
     Generates system prompt
     """
-    return DEFAULT_SYSTEM_MESSAGE + f"Please reply in {language}. "
+    return SIMPLIFY_SYSTEM_MESSAGE + f"Please reply in {language}. "
+
+def _chat_completion(model:str, system_message:str, prompt:str, max_tokens=None, logit_bias=None):
+    """
+    Helper function for chat completion
+    """
+    payload = {
+        "model": model,
+        "messages": [
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": prompt}
+        ],
+        "temperature": 0,
+        "seed": 0
+    }
+    
+    if max_tokens is not None:
+        payload["max_tokens"] = max_tokens
+    
+    if logit_bias is not None:
+        payload["logit_bias"] = logit_bias
+
+    print(payload)
+    completion = client.chat.completions.create(**payload)
+    print(completion)
+    return completion.choices[0].message.content
+
+def _binary_classifier(system_message, prompt):
+    """
+    Helper function to do binary classification
+    """
+    return _chat_completion("gpt-3.5-turbo-1106", system_message, prompt, max_tokens=1, logit_bias={"15": 100, "16": 100})
