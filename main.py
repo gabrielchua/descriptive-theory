847896e485afaa046f16938e33a98d0ab1a5ff27
Gabriel Chua <cyzgab@gmail.com>
2024-01-21 09:13:49 +0800

    change to hello there

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
+"""
+main.py
+"""
 from fastapi import FastAPI
-from pydantic import BaseModel
-
-import uvicorn
-
-import json
-from typing import Any
 
 app = FastAPI()
 
-# Assuming 'nobelprize-laureate.json' is in the same directory as this script
-json_file_path = 'nobelprize-laureate.json'
-
 # Endpoint to serve the JSON data
 @app.get("/")
-async def read_laureates():
-    return "hello world"
+async def hello_world():
+    """
+    Returns hello world
+    """
+    return "hello there"
